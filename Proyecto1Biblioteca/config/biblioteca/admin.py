fded557b1844edd9f4f6a647a51efb04fffe0b0d
Lucas <lippasolli@hotmail.com>
2020-06-12 09:08:13 -0300

    ListDisplay

--- Proyecto1Biblioteca/config/biblioteca/admin.py
+++ Proyecto1Biblioteca/config/biblioteca/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
-admin.site.register(Usuario)
+
+class UsuarioAdmin(admin.ModelAdmin):
+    #fields = ('codigo', 'telefono')
+
+    fieldsets = (
+        ('Informacion Personal',{
+            'fields': ('nombre', 'direccion', 'telefono')
+        }),
+        ('Detalle de alquileres', {
+            'fields': ('ejemplares',)
+        }),
+    )
+    list_display = ['nombre', 'telefono', 'direccion']
+
+admin.site.register(Usuario, UsuarioAdmin)
 admin.site.register(Libro)
 admin.site.register(Ejemplar)
 admin.site.register(Autor)
